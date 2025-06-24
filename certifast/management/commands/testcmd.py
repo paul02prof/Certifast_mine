92c6714138a81386fbbc12ae7959385be9a49327
paul02prof <paul02prof@gmail.com>
2025-06-24 08:03:36 +0100

    step

--- certifast/management/commands/testcmd.py
+++
@@ -1,6 +0,0 @@
-# myapp/management/commands/testcmd.py
-from django.core.management.base import BaseCommand
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        print("✅ Commande test exécutée avec succès !")
